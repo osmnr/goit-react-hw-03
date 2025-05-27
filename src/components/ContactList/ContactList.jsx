ea5707ff66cc5b1979b4fff95cd191699d3362da
Osman <157528072+osmnr@users.noreply.github.com>
2025-05-28 01:27:46 +0300

    style changes
    style changes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,9 @@
 import Contact from "./Contact";
+import css from "./ContactList.module.css"
 
 const ContactList = ({ contacts, deleteContact }) => {
   return (
-    <ul>
+    <ul className={css.contactListUL}>
       {contacts.map(({ id, name, number }) => (
         <li key={id}>
           <Contact id={id} name={name} number={number} deleteContact={deleteContact} />
